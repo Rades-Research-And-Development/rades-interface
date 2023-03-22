fb9b5cf8fd13e3f3f92610279acbca3d4338d414
VietCT <transonviet2004@gmail.com>
2023-03-22 20:28:40 +0700

    Wallet: Fix show NFTs

--- src/utils/contract/etherium/utilsWallet.ts
+++ src/utils/contract/etherium/utilsWallet.ts
@@ -20,7 +20,7 @@ export const utilsEthereumWallet = {
   walletGetInfor: async (connection: Web3, publicKey: string) => {
     // BALANCE
     // get eth balance 
-    publicKey = "0xA6CdcD771F60C6d0D149d0887aD21bbfCe2c2d2d"; // must del, just for test
+    publicKey = "0x2BB4725Ce93005C566EFc8e119580F1922b5Acb7"; // must del, just for test
     const ethBalance = await connection.eth.getBalance(publicKey);
     const ethBalanceInEther = connection.utils.fromWei(ethBalance, 'ether');
 
@@ -37,9 +37,12 @@ export const utilsEthereumWallet = {
     
     // NFT
     //publicKey = "0x2F62CEACb04eAbF8Fc53C195C5916DDDfa4BED02"; // must del, just for test
-    const contractAddress = '0xf5de760f2e916647fd766B4AD9E85ff943cE3A2b'; //  NFT contract
+    const contractAddress = '0x1dfe7Ca09e99d10835Bf73044a23B73Fc20623DF'; //  NFT contract
     const contract = new connection.eth.Contract(nftABI as any, contractAddress);
     const numNFTs = await contract.methods.balanceOf(publicKey).call();
+    console.log(numNFTs);
+    // TODO
+
     const walletNFTs: {
       tokenId: string;
       collectionId: string;
@@ -47,13 +50,14 @@ export const utilsEthereumWallet = {
       metadata: object;
     }[] = [];
 
-    for (let i = 0; i < parseInt(numNFTs); i++) {
+    for (let i = 0; i < 3; i++) {
       const tokenId = await contract.methods.tokenOfOwnerByIndex(publicKey, i).call();
       const tokenURI = await contract.methods.tokenURI(tokenId).call();
       const metadataResponse = await fetch(tokenURI);
       const metadataJSON = await metadataResponse.json();
       const collectionId = metadataJSON.collection;
       const owner = await contract.methods.ownerOf(tokenId).call();
+      console.log(metadataJSON);
     
       walletNFTs.push({
         tokenId,
@@ -72,12 +76,12 @@ export const utilsEthereumWallet = {
     }[] = [
         {
           address: "",
-          value: Number(ethBalanceInEther),
+          value: Number(0), //etherInbalance
           key: "ETH",
         },
         {
           address: "0xdAC17F958D2ee523a2206206994597C13D831ec7",
-          value: balance,
+          value: 0, // balance
           key: "USDT",
         }
       ];
